a67d00c878639ed7276a2a161c6661757daa16b7
mdahirr <m.dahir@hotmail.co.uk>
2022-12-13 12:03:46 +0000

    start of week 6

---
+++ DBFirst_3Layer_TestStarterCode/NorthwindTests/CustomerManagerTests.cs
@@ -0,0 +1,85 @@
+using NUnit.Framework;
+using NorthwindBusiness;
+using NorthwindData;
+using System.Linq;
+
+namespace NorthwindTests
+{
+    public class CustomerTests
+    {
+        CustomerManager _customerManager;
+        [SetUp]
+        public void Setup()
+        {
+            _customerManager = new CustomerManager();
+            // remove test entry in DB if present
+            using (var db = new NorthwindContext())
+            {
+                var selectedCustomers =
+                from c in db.Customers
+                where c.CustomerId == "MANDA"
+                select c;
+
+                db.Customers.RemoveRange(selectedCustomers);
+                db.SaveChanges();
+            }
+        }
+
+        [Test]
+        public void WhenANewCustomerIsAdded_TheNumberOfCustemersIncreasesBy1()
+        {
+
+        }
+
+        [Test]
+        public void WhenANewCustomerIsAdded_TheirDetailsAreCorrect()
+        {
+
+        }
+
+        [Test]
+        public void WhenACustomersDetailsAreChanged_TheDatabaseIsUpdated()
+        {
+
+        }
+
+        [Test]
+        public void WhenACustomerIsUpdated_SelectedCustomerIsUpdated()
+        {
+
+        }
+
+        [Test]
+        public void WhenACustomerIsNotInTheDatabase_Update_ReturnsFalse()
+        {
+
+        }
+
+        [Test]
+        public void WhenACustomerIsRemoved_TheNumberOfCustomersDecreasesBy1()
+        {
+
+        }
+
+        [Test]
+        public void WhenACustomerIsRemoved_TheyAreNoLongerInTheDatabase()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            using (var db = new NorthwindContext())
+            {
+                var selectedCustomers =
+                from c in db.Customers
+                where c.CustomerId == "MANDA"
+                select c;
+
+                db.Customers.RemoveRange(selectedCustomers);
+                db.SaveChanges();
+            }
+        }
+    }
+}
